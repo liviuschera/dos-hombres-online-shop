1887a24e470d169bcc1fa719d990df0f5b41c71b
liviuschera <liviu115@gmail.com>
2019-12-24 14:36:53 +0930

    added sign-in-and-sign-up component and fixed up menu-item.styles

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./sign-in-and-sign-up.styles.scss";
+
+function SignInAndSignUpPage() {
+  return <div className="sign-in-and-sign-up">Sign In</div>;
+}
+
+export default SignInAndSignUpPage;
