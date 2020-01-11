e0b14f52b22194dd6ff2a616fd3b63f13b26922b
liviuschera <liviu115@gmail.com>
2020-01-12 07:34:49 +0930

    ceated the store and added middleware to catch the actions and display them through the logger

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+
+import rootReducer from "./root-reducer";
+
+const middlewares = [logger]; //here you will add as many middlewares as you need
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+export default store;
