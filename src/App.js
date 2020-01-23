9becbae1cbf709f52ffc8a4fc8c9be1432fac0f6
liviuschera <liviu115@gmail.com>
2020-01-23 09:31:12 +0200

    header.component is now using redux to pull currentUser value off of our reducer by using connect() HOC

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
 
   return (
     <div>
-      <Header currentUser={currentUser} />
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { auth } from "../../firebase/firebase.utils";
+import { connect } from "react-redux";
 
 import { ReactComponent as Logo } from "../../assets/shop.svg";
 import "./header.styles.scss";
@@ -34,4 +35,8 @@ function Header({ currentUser }) {
   );
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return { currentUser: state.user.currentUser };
+}
+
+export default connect(mapStateToProps)(Header);
