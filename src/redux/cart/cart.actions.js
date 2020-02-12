9ade11c3383b356d60411573b852f207f640b595
liviuschera <liviu115@gmail.com>
2020-02-12 15:06:35 +0200

    wired the cart cart actions and reducer to ADD TO CART button

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,7 @@ import CartActionTypes from "./cart.types";
 export function toggleCartHidden() {
   return { type: CartActionTypes.TOGGLE_CART_HIDDEN };
 }
+
+export function addItem(item) {
+  return { type: CartActionTypes.ADD_ITEM, payload: item };
+}
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,17 +1,21 @@
 import CartActionTypes from "./cart.types";
 
 const INITIAL_STATE = {
-  hidden: true
+  hidden: true,
+  cartItems: []
 };
 
 export default function cartReducer(state = INITIAL_STATE, action) {
-  // console.log(!state.hidden);
-
   switch (action.type) {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {
         ...state,
         hidden: !state.hidden
+      };
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload]
       };
     default:
       return state;
