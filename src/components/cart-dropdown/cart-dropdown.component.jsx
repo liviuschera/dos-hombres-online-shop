7cf1bee50ffc1b06501793b26f5ce62f87210d80
liviuschera <liviu115@gmail.com>
2020-02-17 17:34:13 +0200

    created cart-item.component and connected our cart-dropdown.component with cart-item.reducer

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,27 @@
 import React from "react";
+// import { useSelector } from "react-redux";
+
 import CustomButton from "../custom-button/custom-button.component";
+import CartItem from "../cart-item/cart-item.component";
 
 import "./cart-dropdown.styles.scss";
+import { useSelector } from "react-redux";
 
 export default function CartDropdown() {
+  // const cartItems = useSelector(state => ({
+  //   cartItems: state
+  // }));
+  const { cartItems } = useSelector(state => ({
+    cartItems: state.cart.cartItems
+  }));
+
   return (
     <div className="cart-dropdown">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {cartItems.map(cartItem => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,6 @@
 import CartActionTypes from "./cart.types";
 
-import { addItemToCart } from "./cart.util";
+import { addItemToCart } from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
