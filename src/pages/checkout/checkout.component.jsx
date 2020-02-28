09566e99aef112d259be9520e43bc062e9e9d8d2
liviuschera <liviu115@gmail.com>
2020-02-28 17:07:45 +0200

    adding HTML elements of the checkout page header

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,5 +3,22 @@ import React from "react";
 import "./checkout.syles.scss";
 
 export default function CheckoutPage() {
-  return <div>CheckoutPage</div>;
+  return (
+    <main className="checkout-page">
+      <header className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
+      </header>
+    </main>
+  );
 }
